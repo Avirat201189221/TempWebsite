ad2f45c264549d87e2a4123bfeee8109fa88b9d8
Avirat201189221 <avirat0603@gmail.com>
2023-07-05 00:30:32 +0530

    nav changes

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('',views.index,name='index'),
+    # path('events',views.events,name='index'),
+    # path('our-team',views.ourTeam,name='index'),
 ]
+
