f56c31cc29e16a00496bacec63e273fac6fd337e
Avirat201189221 <avirat0603@gmail.com>
2023-07-07 23:33:24 +0530

    add events page and their galleries

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('',views.index,name='index'),
-    # path('events',views.events,name='index'),
-    path('our-team',views.ourTeam,name='index'),
+    path('our-team',views.ourTeam,name='ourteam'),
+    path('events',views.events,name='events'),
+    path('colloquium',views.colloquium,name='colloquium'),
+    path('hackonix',views.hackonix,name='hackonix'),
+    path('qoc',views.qoc,name='qoc'),
 ]
 
--- app/views.py
+++ app/views.py
@@ -6,3 +6,15 @@ def index(request):
 
 def ourTeam(request):
     return render(request,"our-team.html")
+
+def events(request):
+    return render(request,"events.html")
+
+def colloquium(request):
+    return render(request,"colloquium.html")
+
+def hackonix(request):
+    return render(request,"hackonix.html")
+
+def qoc(request):
+    return render(request,"qoc.html")
